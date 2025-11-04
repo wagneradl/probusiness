4004d727c182976ee1cc9cb45825a43462e378d4
Wagner Lima <hello@wagnerlima.cc>
2025-11-04 13:40:44 -0300

    feat: adiciona layout raiz do app

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Blueprint Executivo - SEBRAE Pró Business 2025',
+  description: 'Stand SMN | GCPro | IFTI - Teatro Pedra do Reino',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="pt-BR">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
